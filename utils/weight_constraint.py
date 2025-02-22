1480fa53bbe89dcca2a95ed7a63a8eb33e6763e9
孙明博 <1312986353@qq.com>
2025-02-22 12:00:04 +0800

    Initial commit

--- utils/weight_constraint.py
+++
@@ -1,9 +0,0 @@
-class weightConstraint(object):
-    def __init__(self):
-        pass
-
-    def __call__(self, module):
-        if hasattr(module, 'hydropara'):
-            w = module.hydropara.data
-            w = w.clamp(0, 1)
-            module.hydropara.data = w
